5a79728e1f055d10f6c8d30f640f0f9dfd8306aa
Nidarshana Uthpala <abc370643@gmail.com>
2025-06-28 20:30:02 +0530

    launch icon changed and splash changed

--- lib/screen/splash/splash.dart
+++ lib/screen/splash/splash.dart
@@ -2,6 +2,7 @@ import 'package:driving_license_exam/home.dart';
 import 'package:driving_license_exam/screen/login/login.dart';
 import 'package:driving_license_exam/services/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -49,28 +50,86 @@ class _SplashState extends State<Splash> {
                 SizedBox(
                   height: size.height * 0.17,
                 ),
-                Image.asset("assets/images/logo.jpeg", width: size.width * 0.5),
+                // Animated logo with zoom in/zoom out effect
+                Image.asset("assets/images/logo.png", width: size.width * 0.5)
+                    .animate(onPlay: (controller) => controller.repeat())
+                    .scale(
+                      begin: const Offset(1.0, 1.0),
+                      end: const Offset(1.2, 1.2),
+                      duration: const Duration(milliseconds: 1000),
+                      curve: Curves.easeInOut,
+                    )
+                    .then()
+                    .scale(
+                      begin: const Offset(1.2, 1.2),
+                      end: const Offset(1.0, 1.0),
+                      duration: const Duration(milliseconds: 1000),
+                      curve: Curves.easeInOut,
+                    ),
                 SizedBox(
                   height: size.height * 0.1,
                 ),
-                Image.asset("assets/images/Loader.png"),
                 const SizedBox(height: 24),
                 const Center(
                   child: Text(
                     'Wait a second ',
-                    style: TextStyle(fontSize: 24),
+                    style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
                   ),
+                ),
+                const SizedBox(height: 24),
+                const CircularProgressIndicator(
+                  color: Colors.blue,
+                  strokeWidth: 2.0,
                 ),
               ],
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Image.asset(
-                "assets/images/Group 5245.png",
-                width: size.width,
-                fit: BoxFit.cover,
+            // Bottom section with powered by and copyright text
+            Positioned(
+              bottom: 30,
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  const Text(
+                    'Powered by',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  const Text(
+                    'lifewave.lk',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    '© ${DateTime.now().year} TechSolutions Inc. All rights reserved.',
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
               ),
             ),
+            // Align(
+            //   alignment: Alignment.bottomCenter,
+            //   child: Image.asset(
+            //     "assets/images/Group 5245.png",
+            //     width: size.width,
+            //     fit: BoxFit.cover,
+            //   ),
+            // ),
           ],
         ),
       ),
